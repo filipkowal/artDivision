0236f1b94e14c936cdcbf5cbfe3836fe3a53250e
Filip Kowal <filip.kowal@protonmail.com>
2022-09-04 21:13:46 +0200

    Create a grid layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<div class="layout-container">
+	<section><h1>Art Division</h1></section>
+	<slot />
+	<section><h1>Zespół</h1></section>
+</div>
+
+<style>
+	.layout-container {
+		display: grid;
+		grid-template-columns: 1fr 3fr 1fr;
+		grid-template-rows: 1;
+		height: 100vh;
+	}
+	@media (max-width: 1200px) {
+		.layout-container {
+			display: flex;
+			flex-direction: column;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<main>
+	<section class="articles"><h1>Artykuły</h1></section>
+	<section class="offer"><h1>Oferta</h1></section>
+	<section class="workshops"><h1>Szkolenia</h1></section>
+</main>
+
+<style>
+	main {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+		min-height: 100%;
+	}
+
+	@media (max-width: 1200px) {
+		main {
+			display: flex;
+			flex-direction: column;
+		}
+	}
+</style>
