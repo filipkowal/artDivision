adb2414f7aed11c73de3fbeab5946808576018a2
Filip Kowal <filip.kowal@protonmail.com>
2022-09-06 17:47:15 +0200

    Display an article

--- src/routes/artykuly/[slug]/+page.svelte
+++ src/routes/artykuly/[slug]/+page.svelte
@@ -1,4 +1,24 @@
 <script>
 	export let data;
+	const {
+		attributes: { title, tags, date, author, thumbnail, intro },
+		body
+	} = data;
 	console.log(data);
 </script>
+
+<h1>{title}</h1>
+<date>{date}</date>
+<span class="author">{author}</span>
+<ul>
+	{#each tags as tag}
+		<li>{tag}</li>
+	{/each}
+</ul>
+<image src={thumbnail} />
+<h3>{intro}</h3>
+<div>{@html body}</div>
+
+<footer>
+	<p>Jeśli masz jakieś pytania, zapraszamy do <a href="contact">kontaktu</a>.</p>
+</footer>
