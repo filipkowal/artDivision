9b6dc444c595609df45ae36dc02ab459c1192587
Filip Kowal <filip.kowal@protonmail.com>
2022-09-09 14:31:50 +0200

    Create team members pages

---
+++ src/routes/zespol/[slug]/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let data;
+	const { title, body, photo } = data;
+</script>
+
+<article>
+	<a href="/">Powrót</a>
+	<h1>{title}</h1>
+	<div>{@html body}</div>
+	<img src={photo} alt={title} />
+</article>
+
+<style>
+	img {
+		max-width: 300px;
+	}
+</style>
