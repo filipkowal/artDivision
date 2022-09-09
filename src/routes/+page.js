1789fa2fd31b4b3bc3ad4dbb9ad6e384ba2b0088
Filip Kowal <filip.kowal@protonmail.com>
2022-09-09 13:27:00 +0200

    Fetch all posts on Home Page

---
+++ src/routes/+page.js
@@ -0,0 +1,9 @@
+export async function load({ fetch }) {
+	const posts = {
+		artykuly: await fetch(`/api/artykuly`).then((response) => response.json()),
+		oferta: await fetch(`/api/oferta`).then((response) => response.json()),
+		szkolenia: await fetch(`/api/szkolenia`).then((response) => response.json())
+	};
+
+	return posts;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,33 @@
+<script>
+	export let data;
+	const { artykuly, oferta, szkolenia } = data;
+</script>
+
 <div class="section-container">
-	<section class="articles"><a href="artykuly"><h1>Artykuły</h1></a></section>
-	<section class="offer"><a href="oferta"><h1>Oferta</h1></a></section>
-	<section class="workshops"><a href="szkolenia"><h1>Szkolenia</h1></a></section>
+	<section class="articles">
+		<a href="artykuly"><h1>Artykuły</h1></a>
+		{#each artykuly as artykul}
+			<li>
+				<a href="./artykuly/{artykul.fileName}">{artykul.title}</a>
+			</li>
+		{/each}
+	</section>
+	<section class="offer">
+		<a href="oferta"><h1>Oferta</h1></a>
+		{#each oferta as o}
+			<li>
+				<a href="./oferta/{o.fileName}">{o.title}</a>
+			</li>
+		{/each}
+	</section>
+	<section class="workshops">
+		<a href="szkolenia"><h1>Szkolenia</h1></a>
+		{#each szkolenia as szkolenie}
+			<li>
+				<a href="./szkolenia/{szkolenie.fileName}">{szkolenie.title}</a>
+			</li>
+		{/each}
+	</section>
 </div>
 
 <style>
