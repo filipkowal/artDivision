5fa912c3f0c1d87236563a66c28276231eff165d
Filip Kowal <filip.kowal@protonmail.com>
2022-09-11 23:05:05 +0200

    Diffrentiate external links

--- src/components/ContactData.svelte
+++ src/components/ContactData.svelte
@@ -1,7 +1,7 @@
 <address>
 	<p>
-		<a href="facebook.com">Facebook</a><br />
-		<a href="instagram.com">Instagram</a><br />
+		<a href="facebook.com" rel="external">Facebook</a><br />
+		<a href="instagram.com" rel="external">Instagram</a><br />
 		j.sikorska@artdivision.pl<br />
 		+48 509 257 008
 	</p>
