935151619caf55444d4247a58ace93dc985b0257
Filip Kowal <filip.kowal@protonmail.com>
2022-09-21 12:46:00 +0200

    Create 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+	import SectionHeader from '../components/SectionHeader.svelte';
+</script>
+
+<div class="page-container">
+	<SectionHeader title="404" />
+	<div class="content">
+		<h1>Strona nie istnieje</h1>
+		<h2><a href="/">Wróć do strony głównej</a></h2>
+	</div>
+</div>
