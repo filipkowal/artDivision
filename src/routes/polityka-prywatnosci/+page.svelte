72815cc007ba36dec4a91a484214d11a8e10f77e
Filip Kowal <filip.kowal@protonmail.com>
2022-09-21 12:17:21 +0200

    Style privacy a bit

--- src/routes/polityka-prywatnosci/+page.svelte
+++ src/routes/polityka-prywatnosci/+page.svelte
@@ -1,1 +1,10 @@
-<h1>Polityka prywatności</h1>
+<script>
+	import SectionHeader from '../../components/SectionHeader.svelte';
+</script>
+
+<div class="page-container">
+	<SectionHeader title="Polityka prywatności" />
+	<div class="content">
+		<h1>Polityka prywatności</h1>
+	</div>
+</div>
