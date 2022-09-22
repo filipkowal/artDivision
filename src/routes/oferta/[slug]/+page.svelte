f74329f06bed748a7c8a887a0882af8594f56072
Filip Kowal <filip.kowal@protonmail.com>
2022-09-22 09:28:28 +0200

    Add prices to offers

--- src/routes/oferta/[slug]/+page.svelte
+++ src/routes/oferta/[slug]/+page.svelte
@@ -1,9 +1,10 @@
 <script>
 	import ContactForm from '../../../components/ContactForm.svelte';
+	import Prices from '../../../components/Prices.svelte';
 	import SectionHeader from '../../../components/SectionHeader.svelte';
 
 	export let data;
-	const { title, body } = data;
+	const { title, body, price, priceReduced } = data;
 </script>
 
 <article class="column page-container column-2">
@@ -11,6 +12,7 @@
 	<div class="content">
 		<h1>{title}</h1>
 		<div>{@html body}</div>
+		<Prices {price} {priceReduced} />
 		<footer>
 			<p>Jeśli masz jakieś pytania, zapraszamy do <a href="/kontakt"><b>kontaktu</b></a>.</p>
 			<ContactForm name={title} />
