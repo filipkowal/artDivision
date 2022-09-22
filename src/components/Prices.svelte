cb389c4fd054e3f5b5b88aae09e173c18e1b3954
Filip Kowal <filip.kowal@protonmail.com>
2022-09-22 09:26:25 +0200

    Create Prices component

---
+++ src/components/Prices.svelte
@@ -0,0 +1,43 @@
+<script>
+	import SimpleCard from './SimpleCard.svelte';
+
+	export let price, priceReduced;
+</script>
+
+<div class="prices">
+	{#if price}
+		<div class="price-card">
+			<SimpleCard title="Cena regularna"><span class="price">{price}</span></SimpleCard>
+		</div>
+	{/if}
+	{#if priceReduced}
+		<div class="price-card">
+			<SimpleCard title="Cena przy wcześniejszym zapisie"
+				><span class="price">{priceReduced}</span></SimpleCard
+			>
+		</div>
+	{/if}
+</div>
+
+<style>
+	.price {
+		color: #0000ff;
+	}
+	.prices {
+		margin-top: 4rem;
+		display: inline-flex;
+		gap: 1rem;
+	}
+	@media (max-width: 1200px) {
+		.prices {
+			display: flex;
+			flex-direction: column;
+			gap: 0;
+		}
+	}
+	.price-card {
+		width: 23rem;
+		max-width: 100%;
+		height: 12rem;
+	}
+</style>
--- src/routes/szkolenia/[slug]/+page.svelte
+++ src/routes/szkolenia/[slug]/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import ContactForm from '../../../components/ContactForm.svelte';
+	import Prices from '../../../components/Prices.svelte';
 	import SectionHeader from '../../../components/SectionHeader.svelte';
-	import SimpleCard from '../../../components/SimpleCard.svelte';
 
 	export let data;
 	const { title, body, price, priceReduced, level } = data;
@@ -13,20 +13,7 @@
 		<h1>{title}</h1>
 		<span class="level">{level}</span>
 		<div>{@html body}</div>
-		<div class="prices">
-			{#if price}
-				<div class="price-card">
-					<SimpleCard title="Cena regularna"><span class="price">{price}</span></SimpleCard>
-				</div>
-			{/if}
-			{#if priceReduced}
-				<div class="price-card">
-					<SimpleCard title="Cena przy wcześniejszym zapisie"
-						><span class="price">{priceReduced}</span></SimpleCard
-					>
-				</div>
-			{/if}
-		</div>
+		<Prices {price} {priceReduced} />
 		<footer>
 			<h2>Formularz do zapisów</h2>
 			<ContactForm name={title} />
@@ -35,31 +22,14 @@
 </article>
 
 <style>
+	.level {
+		color: #0000ff;
+	}
 	h1 {
 		font-weight: 700;
-	}
-	.price {
-		color: #0000ff;
 	}
 	footer {
 		border-top: 1px solid black;
 		margin-top: 5rem;
-	}
-	.prices {
-		margin-top: 4rem;
-		display: inline-flex;
-		gap: 1rem;
-	}
-	@media (max-width: 1200px) {
-		.prices {
-			display: flex;
-			flex-direction: column;
-			gap: 0;
-		}
-	}
-	.price-card {
-		width: 23rem;
-		max-width: 100%;
-		height: 12rem;
 	}
 </style>
