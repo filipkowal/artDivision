7a434ac6598fd27b7f53017c23878b9cb0990b09
Filip Kowal <filip.kowal@protonmail.com>
2022-10-13 22:28:37 +0200

    Add Privacy Policy content

--- src/routes/polityka-prywatnosci/+page.svelte
+++ src/routes/polityka-prywatnosci/+page.svelte
@@ -10,6 +10,181 @@
 		<SectionHeader title="Polityka prywatności" />
 		<div class="content">
 			<h1>Polityka prywatności</h1>
+
+			<ol>
+				<li>
+					Kto jest administratorem państwa danych?<br />
+					Administratorem Pani/ Pana danych osobowych jest FUNDACJA ROZWOJU PRZEDSIĘBIORCZOŚCI „TWÓJ
+					STARTUP” z siedzibą w Warszawie (adres siedziby: ul. Żurawia 6/12, lok. 766, 00-503 Warszawa).
+					<br /><br />
+
+					Powyższa polityka prywatności dotyczy usług Fundacji świadczonych przez zorganizowaną
+					część przedsiębiorstwa …………………………. dane kontaktowe: numer telefonu ………………………………., adres
+					e-mail: …………………………………. realizowanych przez …………….. (imię i nazwisko Beneficjenta)
+				</li>
+
+				<li>
+					Dlaczego przetwarzamy Państwa Dane?
+					<ol>
+						<li>
+							W związku z usługami świadczonymi za pośrednictwem …………………. polegającymi na ……………………..
+							Tj. w celu zawarcia lub wykonania umowy (art. 6 ust. 1 lit.b Rozporządzenia), Państwa
+							dane będą przechowywane do czasu wykonania umowy
+						</li>
+						<li>
+							W celach marketingowych i promocji produktów oferowanych przez Fundację Rozwoju
+							Przedsiębiorczości „Twój StartUp” z siedzibą w Warszawie będących naszym prawnie
+							uzasadnionym interesem (art. 6 ust. 1 lit. f Rozporządzenia). Państwa dane będą
+							przetwarzane do czasu wyrażenia sprzeciwu ( jeśli nie wysyłają Państwo informacji
+							handlowych, proszę o usunięcie tego punktu).
+						</li>
+						<li>
+							Wypełniania wszelkich ciążących na nas obowiązków prawnych w związku z podjętą
+							współpracą (art. 6 ust. 1 lit. c Rozporządzenia), cel ten związany jest z obowiązkami
+							prawnymi przechowania określonych dokumentów przez czas wskazany w przepisach prawa,
+							np.:
+							<ul>
+								<li>
+									art. 74 ust. 2 pkt 1-8 Ustawy z 29.09.1994 r. o rachunkowości (Dz. U. z 2017 r.
+									poz. 1858);
+								</li>
+								<li>
+									art. 8b ust. 3 w zw. z art. 9k Ustawy z dnia 16.11.2000r. o przeciwdziałaniu
+									praniu pieniędzy oraz finansowaniu terroryzmu (Dz. U. 2000 nr 116 poz. 1216);
+								</li>
+								<li>
+									art. 86 §1 i 2 Ustawy z dnia 29 sierpnia 1997 r. - Ordynacja podatkowa (Dz. U.
+									1997 nr 137 poz. 926).
+								</li>
+							</ul>
+						</li>
+						<li>
+							W celu ewentualnego ustalenia, dochodzenia lub obrony przed roszczeniami, co jest
+							naszym prawnie uzasadnionym interesem (podstawa z art. 6 ust. 1 lit. f)
+							Rozporządzenia). Dane będą przetwarzane do czasu przedawnienia roszczeń
+						</li>
+						<li>
+							W przypadku wyrażenia zgody, Państwa dane będą przetwarzane w granicach i celu
+							wskazanym w wyrażonej zgodzie (podstawa z art. 6 ust. 1 lit. a). Dane będą
+							przetwarzane do czasu wycofania zgody
+						</li>
+					</ol>
+				</li>
+				<li>
+					Jakie dane podlegają przetwarzaniu? Do zawarcia umowy wymagamy podania przez danych na
+					formularzu umowy / zamówienia (jeśli ich Państwo nie podadzą, nie zawrzemy umowy/ nie
+					wykonamy zamówienia). Dodatkowo możemy poprosić o dane opcjonalne, które nie mają wpływu
+					na zawarcie umowy (jeśli ich nie otrzymamy, nie będziemy mogli, np. dzwonić pod numer
+					kontaktowy). W trakcie trwania umowy, świadcząc usługi, wchodzimy w posiadanie innych
+					Pani/Pana danych. Pojawienie się u nas Pani/Pana danych osobowych jest konsekwencją
+					działania naszych usług, z których Pan/Pani korzysta.
+				</li>
+				<li>
+					Komu zostaną udostępnione dane?
+					<ol>
+						<li>
+							Organy władzy publicznej, w zakresie, w którym nie otrzymują danych w ramach
+							konkretnego postępowania na podstawie prawa;
+						</li>
+						<li>
+							Podmioty, które przetwarzają Pana/Pani dane osobowe w imieniu Fundacji na podstawie
+							zawartej z Fundacją umowy powierzania przetwarzania danych osobowych (tzw. podmioty
+							przetwarzające). Będą to, m.in.: Beneficjenci Fundacji, informatycy, firmy
+							archiwizujące, podmioty świadczące usługi bankowe i płatnicze, firmy hostingowe;
+						</li>
+						<li>
+							Zewnętrzni administratorzy danych (tzw. Równoległy Administrator, któremu dane są
+							udostępniane, np. radcy prawni i adwokaci, podmioty prowadzące działalność kurierską
+							lub pocztową, podmioty nabywające wierzytelności - w razie niezapłacenia przez Państwa
+							naszych faktur w terminie),
+						</li>
+						<li>
+							Odbiorcy w państwie trzecim lub organizacji międzynarodowej, tj. …………………………. Podstawą
+							transferu Państwa danych jest/są
+							<ul>
+								<li>Wyrażona przez Państwa zgoda</li>
+								<li>Konieczność w celu wykonania umowy</li>
+								<li>Standardowe klauzule ochrony danych zatwierdzone przez Komisję Europejską</li>
+								<li>
+									Zatwierdzony mechanizmy certyfikacji w połączeniu z egzekwowalnymi zobowiązaniami
+									odbiorcy danych do odpowiednich zabezpieczeń
+								</li>
+								<li>
+									Decyzja Komisji Europejskiej o zapewnieniu odpowiedniego stopnia ochrony w Kraju
+								</li>
+							</ul>
+						</li>
+					</ol>
+				</li>
+				<li>
+					Czy Dane są profilowane?<br />
+					Informujemy, iż Państwa dane nie będą podlegać profilowaniu, czyli zautomatyzowanej analizy
+					Pana/Pani danych i opracowania przewidywań na temat preferencji lub przyszłych zachowań (profilowanie
+					oznacza, np. w przypadku profilowania marketingowego określanie, którą ofertą może być Pan/Pani
+					najbardziej zainteresowany/a w oparciu o wcześniejsze Pana/Pani wybory).
+				</li>
+				<li>
+					Jakie przysługują Państwu prawa?
+					<ol>
+						<li>
+							Prawo dostępu do przetwarzanych przez nas danych osobowych (art. 15 Rozporządzenia),
+						</li>
+						<li>
+							Prawo do sprostowania powierzonych danych osobowych, w tym ich poprawianie (art. 16
+							Rozporządzenia),
+						</li>
+						<li>
+							Prawo do usunięcia danych osobowych z naszych systemów tzw. „prawo do bycia
+							zapomnianym” - jeżeli Państwa zdaniem nie ma podstaw do tego, abyśmy przetwarzali
+							Państwa dane, mogą Państwo zażądać, abyśmy je usunęli (art. 17 Rozporządzenia),
+						</li>
+						<li>
+							Prawo do ograniczenia przetwarzania danych osobowych - mogą Państwo zażądać, abyśmy
+							ograniczyli przetwarzanie danych osobowych wyłącznie do ich przechowywania lub
+							wykonywania uzgodnionych z Państwem działań, jeżeli posiadamy nieprawidłowe dane na
+							Państwa temat lub przetwarzamy je bezpodstawnie; lub nie chcą Państwo, żebyśmy je
+							usunęli, bo są Państwu potrzebne do ustalenia, dochodzenia lub obrony roszczeń; lub na
+							czas wniesionego przez sprzeciwu względem przetwarzania danych (art. 18
+							Rozporządzenia),
+						</li>
+						<li>
+							Prawo do przenoszenia danych – Mają Państwo prawo otrzymać od nas w ustrukturyzowanym,
+							powszechnie używanym formacie nadającym się do odczytu maszynowego (np. format „.csv”)
+							dane osobowe dotyczące Państwa, w których posiadaniu jesteśmy na podstawie umowy lub
+							wyrażonej zgody. Uprawnienie to będzie przysługiwało, gdy będziemy posiadać dane w
+							formacie elektronicznym – w przypadku posiadania danych jedynie w formie papierowej
+							nie będą Państwo mieli możliwości skorzystania z tego uprawnienia. Mogą Państwo zlecić
+							nam przesłanie tych danych bezpośrednio innemu podmiotowi (art. 20 Rozporządzenia),
+						</li>
+						<li>
+							Prawo do cofnięcia zgody na przetwarzanie danych osobowych -w każdej chwili mają
+							Państwo prawo cofnąć zgodę na przetwarzanie danych osobowych, które przetwarzamy na
+							podstawie zgody – art. 7 ust. 3 Rozporządzenia. Cofnięcie zgody nie będzie wpływać na
+							zgodność z prawem przetwarzania, którego dokonano na podstawie zgody przed jej
+							wycofaniem. Cofnięcie zgody występuje poprzez wysłanie wiadomości mailowej na adres:
+							……………….(proszę o uzupełnienie)
+						</li>
+						<li>
+							Prawo do sprzeciwu - może Pan/Pani wnieść sprzeciw wobec przetwarzania Pana/Pani
+							danych, jeśli podstawą wykorzystania danych jest nasz prawnie uzasadniony interes -
+							art. 21 Rozporządzenia. W takiej sytuacji, po rozpatrzeniu Państwa wniosku, nie
+							będziemy już mogli przetwarzać danych osobowych objętych sprzeciwem na tej podstawie,
+							chyba, że wykażemy, istnienie prawnie uzasadnionych podstaw do przetwarzania danych,
+							które uznaje się za nadrzędne wobec Państwa interesów, praw i wolności
+						</li>
+						<li>
+							Jeżeli w Państwa opinii przetwarzanie danych osobowych narusza przepisy
+							Rozporządzenia, przysługuje Państwu prawo do wniesienia skargi do organu nadzorczego
+							tj. Prezesa Urzędu Ochrony Danych Osobowych
+						</li>
+					</ol>
+				</li>
+				<li>
+					Kontakt<br />
+					Jeśli potrzebujecie Państwo dodatkowych informacji związanych z ochroną danych osobowych lub
+					chcecie skorzystać z przysługujących praw, prosimy o kontakt na adres e-mail: rodo@twojstartup.pl
+				</li>
+			</ol>
 		</div>
 	</div>
 </PageTransition>
