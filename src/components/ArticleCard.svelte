9bc7421e3b25f3972837266cd108b00a7d840ee3
Nadia Tumidajska <nadia.tumidajska@gmail.com>
2025-03-24 14:51:32 +0100

    hoover fis

--- src/components/ArticleCard.svelte
+++ src/components/ArticleCard.svelte
@@ -3,9 +3,15 @@
 	import { trailingOrphansToNewLine } from '../lib/utils';
 
 	export let title, thumbnail, fileName, tags;
+
+	function enableHoverEffect(event) {
+		const target = event.currentTarget;
+		target.classList.add('hover-active');
+		setTimeout(() => target.classList.remove('hover-active'), 1000);
+	}
 </script>
 
-<a href={`/artykuly/${fileName}`}>
+<a href={`/artykuly/${fileName}`} on:touchstart={enableHoverEffect}>
 	<div class="articleCard">
 		<div class="image-container">
 			<img src={thumbnail + '?nf_resize=smartcrop&w=160&h=200'} alt={title} />
@@ -18,9 +24,6 @@
 </a>
 
 <style>
-	:root {
-		--url: url(--imageUrl);
-	}
 	.articleCard {
 		margin-bottom: 3.57rem;
 	}
@@ -30,9 +33,6 @@
 		font-size: 2.1rem;
 		line-height: 2.1rem;
 		font-style: italic;
-	}
-	:global(.articleCard:hover .tag) {
-		border-color: #0000ff;
 	}
 	.image-container {
 		width: 160px;
@@ -45,12 +45,24 @@
 		height: 100%;
 		object-fit: cover;
 	}
-	a:hover img {
+
+	@media (hover: hover) {
+		a:hover img {
+			mix-blend-mode: screen;
+			-webkit-filter: grayscale(100%);
+			filter: grayscale(100%);
+		}
+		a:hover .image-container {
+			background: #0000ff;
+		}
+	}
+
+	.hover-active .image-container {
+		background: #0000ff;
+	}
+	.hover-active img {
 		mix-blend-mode: screen;
 		-webkit-filter: grayscale(100%);
 		filter: grayscale(100%);
-	}
-	a:hover .image-container {
-		background: #0000ff;
 	}
 </style>
